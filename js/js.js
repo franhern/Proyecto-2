b0dc04199111be1ec4fa02610cd4b7e83ca24e7d
unknown <franhernandezsarabia@gmail.com>
2022-12-03 22:33:54 -0500

    primer

---
+++ js/js.js
@@ -0,0 +1,16 @@
+let products = [];
+let total = 0;
+
+
+function add(product, price){
+    console.log(product,price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("pago").innerHTML = `Pagar $${total}`;
+
+}
+
+function pay(){
+    console.log(product,price);
+    window.alert(products.join(",\n"));
+}
